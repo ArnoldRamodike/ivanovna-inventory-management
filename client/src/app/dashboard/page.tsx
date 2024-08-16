f269701d219ecf3a165659df3096ca44ac7add04
ArnoldRamodike <riccoarnold23@gmail.com>
2024-08-16 13:42:52 +0200

    Styles updates with layout and rating component

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+
 import React from "react";
+import CardPoularProducts from "./CardPopularProducts";
+import CardSalesSummury from "./CardSalesSummury";
+import CardPurchaseSummury from "./CardPurchaseSummury";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      <CardPoularProducts />
+      <CardSalesSummury />
+      <CardPurchaseSummury />
+      <div className="row-span-3 bg-gray-500"></div>
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-500"></div>
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-500"></div>
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-500"></div>
+    </div>
+  );
 };
 
 export default Dashboard;
