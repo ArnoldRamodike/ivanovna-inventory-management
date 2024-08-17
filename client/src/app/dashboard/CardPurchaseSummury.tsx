a6c807c364aed90ca1221b01f94489a0817a6060
ArnoldRamodike <riccoarnold23@gmail.com>
2024-08-17 11:30:18 +0200

    Purchase component fully

---
+++ client/src/app/dashboard/CardPurchaseSummury.tsx
@@ -0,0 +1,96 @@
+import { useGetDashboardMetricsQuery } from "@/state/api";
+import { TrendingDown, TrendingUp } from "lucide-react";
+import numeral from "numeral";
+import React from "react";
+import {
+  Area,
+  AreaChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+const CardPurchaseSummury = () => {
+  const { data, isLoading } = useGetDashboardMetricsQuery();
+  const purchaseData = data?.purchaseSummury || [];
+
+  const lastDatapoint = purchaseData[purchaseData.length - 1] || null;
+  return (
+    <div className="flex flex-col justify-between row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-white shadow-md rounded-2xl">
+      {isLoading ? (
+        <div className="m-5">Loading...</div>
+      ) : (
+        <>
+          {/* Header */}
+          <div className="">
+            <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
+              Purchase Summury
+            </h2>
+          </div>
+          {/* Body */}
+          <div className="">
+            <div className="mb-4 mt-7 px-7">
+              <p className="text-xs text-gray-400">Purchased</p>
+              <div className="flex items-center ">
+                <p className="text-2xl font-bold">
+                  {lastDatapoint
+                    ? numeral(lastDatapoint.totalPurchased).format("$0.00a")
+                    : "0"}
+                </p>
+                {lastDatapoint && (
+                  <p
+                    className={`text-sm ${
+                      lastDatapoint.changePercentage! >= 0
+                        ? "text-green-500"
+                        : "text-red-500"
+                    } flex ml-3 `}
+                  >
+                    {lastDatapoint.changePercentage! >= 0 ? (
+                      <TrendingUp className="h-5 w-5 mr-1" />
+                    ) : (
+                      <TrendingDown className="h-5 w-5 mr-1" />
+                    )}
+                    {Math.abs(lastDatapoint.changePercentage!)}%
+                  </p>
+                )}
+              </div>
+            </div>
+            {/* Charts */}
+            <ResponsiveContainer width="100%" height={350} className="px-7">
+              <AreaChart
+                data={purchaseData}
+                margin={{ top: 0, right: 0, left: -50, bottom: 45 }}
+              >
+                <XAxis dataKey="date" tick={false} axisLine={false} />
+                <YAxis tick={false} tickLine={false} axisLine={false} />
+                <Tooltip
+                  formatter={(value: number) => [
+                    `$${value.toLocaleString("en")}`,
+                  ]}
+                  labelFormatter={(label) => {
+                    const date = new Date(label);
+                    return date.toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    });
+                  }}
+                />
+                <Area
+                  type="linear"
+                  dataKey="totalPurchased"
+                  stroke="#8884D8"
+                  fill="#3182ce"
+                  dot={true}
+                />
+              </AreaChart>
+            </ResponsiveContainer>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default CardPurchaseSummury;
