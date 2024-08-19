d908599d51d83feb806ee2abeca13f815d9e6e0f
ArnoldRamodike <riccoarnold23@gmail.com>
2024-08-19 10:54:38 +0200

    products inventory crude and fixes

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -9,8 +9,9 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
-// Imorts Routes
-const dashboardController_1 = require("./controllers/dashboardController");
+// Imports Routes
+const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
+const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
 // Configurations
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -25,7 +26,8 @@ app.use((0, cors_1.default)());
 app.get("/hello", (req, res) => {
     res.send("We are here baby");
 });
-app.use("/dashboard", dashboardController_1.getDashboardMetrics);
+app.use("/dashboard", dashboardRoutes_1.default);
+app.use("/products", productRoutes_1.default);
 // Server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,31 +6,33 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 
 
-// Imorts Routes
-import { getDashboardMetrics } from "./controllers/dashboardController";
+// Imports Routes
+import dashboardRoutes from "./routes/dashboardRoutes";
+import productRoutes from "./routes/productRoutes"
 // Configurations
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin" }));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cors( ));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 // Routes
 app.get("/hello", (req, res) => {
     res.send("We are here baby")
 })
 
-app.use("/dashboard", getDashboardMetrics)
+app.use("/dashboard", dashboardRoutes)
+app.use("/products", productRoutes)
 
 // Server
 
-const port = process.env.PORT  || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Sever starteed on port: ${port}`);
-    
+
 })
